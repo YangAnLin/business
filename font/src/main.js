1e16add8b3f890d2216ac0ff035d2bc40273de28
anthony <y950727@gmail.com>
2020-05-13 16:42:54 +0800

    基础框架和调试

--- font/src/main.js
+++ font/src/main.js
@@ -2,17 +2,42 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+//==========================全局样式====================================================
+import './assets/css/global.css'
+
+//==========================element====================================================
 /* 引入element 组件 */
 import './plugins/element.js'
 /* 引入element 样式 */
 import 'element-ui/lib/theme-chalk/index.css'
-import './assets/css/global.css'
+
+//==========================axios====================================================
 /* 请求数据框架 */
 import axios from 'axios'
 // 配置请求的根路径
-axios.defaults.baseURL = 'http://localhost:10001/'
+axios.defaults.baseURL = 'http://api.yanganlin.com/'
 /* 配置成全局的 */
 Vue.prototype.$http = axios
+
+//==========================进度条====================================================
+// 导入进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+
+//==========================拦截器和进度条====================================================
+axios.interceptors.request.use(config => {
+  NProgress.start();  // 配置进度条
+  // config.headers.Authorization =  window.sessionStorage.getItem("token")
+  return config
+})
+
+axios.interceptors.response.use(config => {
+  NProgress.done(); // 配置进度条
+  return config
+})
+
 
 Vue.config.productionTip = false
 
--- font/src/router/index.js
+++ font/src/router/index.js
@@ -20,15 +20,32 @@ const routes = [
     {
         path: '/about',
         name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
 ]
 
+
 const router = new VueRouter({
     routes
+})
+
+//==========================路由守卫====================================================
+// to将要访问的路径
+// from 从哪个路径跳转而来
+// next 是一个函数,表示方形 ,比如 netxt()   和 next("/login)强制跳转
+router.beforeEach((to,from,next)=>{
+
+    // 访问首页不用验证
+    if (to.path === '/') {
+        return next()
+    }
+
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) {
+        return next('/login')
+    }else{
+        next()
+    }
 })
 
 export default router
